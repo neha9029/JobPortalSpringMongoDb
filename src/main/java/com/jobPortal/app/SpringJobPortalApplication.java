14d7b374cfe12bb8a682b90fda8f84afabb66df1
neha9029 <nehanaik9029@gmail.com>
2019-01-23 19:32:00 -0500

    Spring boot project setup

---
+++ src/main/java/com/jobPortal/app/SpringJobPortalApplication.java
@@ -0,0 +1,14 @@
+package com.jobPortal.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJobPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJobPortalApplication.class, args);
+	}
+
+}
+
